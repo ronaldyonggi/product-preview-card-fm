26d0f206c9e05e1f2cb7fd658a913e48a49e2c52
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-03 11:02:39 +0800

    define ProductCard component

---
+++ src/components/ProductCard/ProductCard.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import { IProduct } from '../../types';
+import styles from './ProductCard.module.css';
+import AddToCardButton from './Button/AddToCardButton';
+
+interface ProductCardProps {
+  product: IProduct;
+}
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return (
+    <div className={styles.container}>
+      <img
+        src={width <= 375 ? product.image.mobile : product.image.desktop}
+        alt='Image of product'
+        className={styles.image}
+      />
+      <div className={styles.content}>
+        <p className={styles.category}>{product.category}</p>
+        <h1 className={styles.name}>{product.name}</h1>
+        <p className={styles.description}>{product.description}</p>
+
+        <div className={styles.priceContainer}>
+          <p className={styles.price}>${product.price}</p>
+          <p className={styles.originalPrice}>${product.originalPrice}</p>
+        </div>
+        <div className={styles.buttonContainer}>
+          <AddToCardButton />
+        </div>
+      </div>
+    </div>
+  );
+}
