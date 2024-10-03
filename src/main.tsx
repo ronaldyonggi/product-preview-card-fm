d56396e87e214b2304893c0c77711a939fafc406
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-03 11:04:01 +0800

    update main to reflect desired app

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,30 @@
-import { createRoot } from 'react-dom/client'
+import './styles/reset.css'
+import './styles/global.css'
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+import { createRoot } from 'react-dom/client';
+import ProductCard from './components/ProductCard/ProductCard';
+import imageProductMobile from './assets/images/image-product-mobile.jpg';
+import imageProductDesktop from './assets/images/image-product-desktop.jpg';
+
+const product = {
+  name: 'Gabrielle Essence Eau De Parfum',
+  category: 'Perfume',
+  image: {
+    mobile: imageProductMobile,
+    desktop: imageProductDesktop,
+  },
+  description:
+    'A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.',
+  price: 149.99,
+  originalPrice: 169.99,
+};
+
+export function App() {
+  return (
+    <main>
+      <ProductCard product={product} />
+    </main>
+  );
+}
+
+createRoot(document.getElementById('root')!).render(<App />);
